eebc704dc43964957da3774c85fd6d9524cf0829
Demka <GeorgiyDemo@users.noreply.github.com>
2021-05-11 19:55:08 +0300

    .idea, базовая логика в Main

--- client.py
+++ client.py
@@ -4,11 +4,17 @@ import pickle
 HOST = '127.0.0.1'
 PORT = 8080
 
-sock = socket.socket()
-sock.connect((HOST, PORT))
 
-p, g, a = 7, 5, 3
-A = g ** a % p
-sock.send(pickle.dumps((p, g, A)))
+def main():
+    sock = socket.socket()
+    sock.connect((HOST, PORT))
 
-sock.close()
+    p, g, a = 7, 5, 3
+    A = g ** a % p
+    sock.send(pickle.dumps((p, g, A)))
+
+    sock.close()
+
+
+if __name__ == "__main__":
+    main()
--- server.py
+++ server.py
@@ -4,12 +4,18 @@ import pickle
 HOST = '127.0.0.1'
 PORT = 8080
 
-sock = socket.socket()
-sock.bind((HOST, PORT))
-sock.listen(1)
-conn, addr = sock.accept()
 
-msg = conn.recv(1024)
-print(pickle.loads(msg))
+def main():
+    sock = socket.socket()
+    sock.bind((HOST, PORT))
+    sock.listen(1)
+    conn, addr = sock.accept()
 
-conn.close()
+    msg = conn.recv(1024)
+    print(pickle.loads(msg))
+
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
